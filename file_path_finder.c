6df39db5548380e2a055d82a3b117538c1fc6789
Gideon Oba <deyonoba@gmail.com>
2023-10-16 21:06:01 +0000

    add file_path_finder.c

---
+++ file_path_finder.c
@@ -0,0 +1,65 @@
+#include "shell.h"
+
+/**
+ * get_path_dir - Get Path directories
+ * @env: Environment variable
+ *
+ * Return: Array of pointer
+ */
+char **get_path_dir(char **env)
+{
+	int i, var_count, path_count;
+	char *result;
+
+	for (i = 0; env[i] != NULL; i++)
+	{
+		result = strstr(env[i], "PATH");
+
+		if (result != NULL)
+		{
+			char **varname_val = arg_parser(env[i], &var_count, "=");
+			char **paths = arg_parser(varname_val[1], &path_count, ":");
+
+			free_array(varname_val);
+			return (paths);
+		}
+	}
+	return (NULL);
+}
+
+/**
+ * get_file_path - Get path to executable file
+ * @command: Command string
+ * @env: Array of pointer to environment variables
+ *
+ * Return: Path
+ */
+char *get_file_path(char *command, char **env)
+{
+	char **paths = get_path_dir(env);
+	int i;
+	struct stat fstatus;
+	char test_buffer[1000], *path = NULL;
+	
+
+	for (i = 0; paths[i] != NULL; i++)
+	{
+
+		strcpy(test_buffer, paths[i]);
+		strcat(test_buffer, "/");
+		strcat(test_buffer, command);
+
+		if (stat(test_buffer, &fstatus) == 0)
+		{
+			path = strdup(test_buffer);
+			break;
+		}
+		else if (stat(command, &fstatus) == 0)
+		{
+			path = strdup(command);
+			break;
+		}
+	}
+	free_array(paths);
+	return (path);
+}
