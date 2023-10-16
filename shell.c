8d1f6d70bed5840092b566ce602448840d96f828
Gideon Oba <deyonoba@gmail.com>
2023-10-16 21:07:26 +0000

    add shell.c

---
+++ shell.c
@@ -0,0 +1,19 @@
+#include "shell.h"
+
+/**
+ * main - Execute Shell
+ *
+ * @argc: Argument count
+ * @argv: Argument vector
+ * @env: Environment
+ *
+ * Return: Integer
+ */
+int main(int argc, char **argv, char **env)
+{
+	(void)argc;
+	if (isatty(STDIN_FILENO))
+		return (interactive_mode(argv[0], env));
+
+	return (non_interactive_mode(argv[0], env));
+}
