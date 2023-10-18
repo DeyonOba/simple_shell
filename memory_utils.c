697e3820be7f3b8dc870aa03c763b021595d1419
Gideon Oba <deyonoba@gmail.com>
2023-10-18 14:19:40 +0000

    modified: memory_utils.c

--- memory_utils.c
+++ memory_utils.c
@@ -15,3 +15,39 @@ void free_array(char **array, int array_count)
 	}
 	free(array);
 }
+
+void *_realloc(void *ptr, size_t size)
+{
+	void *rptr;
+	size_t new_size;
+
+	/* condition equivalent to free()*/
+	if (size != 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	/* condition equivalent to malloc()*/
+	if (ptr == NULL)
+	{
+		return (malloc(size));
+	}
+
+	rptr = malloc(size);
+
+	/* return pointer if reallocation failed*/
+	if (rptr == NULL)
+		return (ptr);
+
+	/* Borrowed code for copying size of memory*/
+	/*strcpy not working for void type*/
+	new_size = (size < sizeof(ptr)) ? size : sizeof(ptr);
+
+	memcpy(rptr, ptr, new_size);
+
+	free(ptr);
+	return (rptr);
+}
+
+
